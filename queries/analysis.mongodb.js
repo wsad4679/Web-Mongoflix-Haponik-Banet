d795b114eb9f2c9331325fe6cc954df7107d3ce6
szymonh <szymon.haponik@cyfroweszkoly.pl>
2026-02-24 14:31:53 +0100

    Create query for finding action movies after 2000 year

--- queries/analysis.mongodb.js
+++ queries/analysis.mongodb.js
@@ -1,1 +1,26 @@
+db = db.getSiblingDB('mongoflix');
+
+print('--- Raport bazy mongoflix ---');
+
+printjson(
+    '--- Filmy akcji wydane po roku 2000 ---\n',
+    db.movies.aggregate([
+        {
+            $match:
+                {
+                    genres: {
+                        $in: ["Action"]
+                    },
+                    releaseDate: {
+                        $gte: "2000"
+                    }
+                }
+        },
+        {
+            $project: {
+                title: 1, _id:0, genres: 1, releaseDate: 1
+            }
+        }
+    ])
+)
 
