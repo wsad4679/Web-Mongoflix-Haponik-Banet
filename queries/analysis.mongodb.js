6b16b1f0f3f997d1bd8c15eb38da957dcce6ce53
szymonh <szymon.haponik@cyfroweszkoly.pl>
2026-02-25 09:03:29 +0100

    Move query to consts and improve result format in console

--- queries/analysis.mongodb.js
+++ queries/analysis.mongodb.js
@@ -2,115 +2,61 @@ db = db.getSiblingDB('mongoflix');
 
 print('--- Raport bazy mongoflix ---');
 
+const moviesAfter2000YearQuery = db.movies.aggregate([
+    {
+        $match:
+            {
+                genres: {
+                    $in: ["Action"]
+                },
+                releaseDate: {
+                    $gte: "2000"
+                }
+            }
+    },
+    {
+        $project: {
+            title: 1, _id:0, genres: 1, releaseDate: 1
+        }
+    }
+]).toArray()
+
 printjson(
-    '--- Filmy akcji wydane po roku 2000 ---\n',
-    db.movies.aggregate([
+    '\n\n--- Filmy akcji wydane po roku 2000 ---\n'
+)
+
+console.table(moviesAfter2000YearQuery);
+
+
+const avgRatingQuery = db.movies.aggregate(
+    [
         {
-            $match:
+            $lookup:
+            /**
+             * from: The target collection.
+             * localField: The local join field.
+             * foreignField: The target join field.
+             * as: The name for the results.
+             * pipeline: Optional pipeline to run on the foreign collection.
+             * let: Optional variables to use in the pipeline field stages.
+             */
                 {
-                    genres: {
-                        $in: ["Action"]
-                    },
-                    releaseDate: {
-                        $gte: "2000"
-                    }
+                    from: "reviews",
+                    localField: "_id",
+                    foreignField: "movieId",
+                    as: "userReviews"
                 }
         },
         {
-            $project: {
-                title: 1, _id:0, genres: 1, releaseDate: 1
-            }
-        }
-    ])
-)
-
-
-
-printjson(
-    "--- Ocena krytyków oraz średnia ocen użytkowników ---\n",
-
-    db.movies.aggregate(
-        [
-            {
-                $lookup:
-                /**
-                 * from: The target collection.
-                 * localField: The local join field.
-                 * foreignField: The target join field.
-                 * as: The name for the results.
-                 * pipeline: Optional pipeline to run on the foreign collection.
-                 * let: Optional variables to use in the pipeline field stages.
-                 */
-                    {
-                        from: "reviews",
-                        localField: "_id",
-                        foreignField: "movieId",
-                        as: "userReviews"
-                    }
-            },
-            {
-                $unwind:
-                /**
-                 * path: Path to the array field.
-                 * includeArrayIndex: Optional name for index.
-                 * preserveNullAndEmptyArrays: Optional
-                 *   toggle to unwind null and empty values.
-                 */
-                    {
-                        path: "$userReviews"
-                    }
-            },
-            {
-                $group:
-                /**
-                 * _id: The id of the group.
-                 * fieldN: The first field name.
-                 */
-                    {
-                        _id: "$title",
-                        criticsRating: {
-                            $first: "$reviewScore"
-                        },
-                        avgUserRating: {
-                            $avg: "$userReviews.reviewScore"
-                        }
-                    }
-            }
-        ])
-
-)
-
-
-printjson(
-    "--- Łączne wyświetlenia zdobyte przez filmy danych reżyserów ---\n",
-    db.movies.aggregate([
-        {
-            $group:
+            $unwind:
             /**
-             * _id: The id of the group.
-             * fieldN: The first field name.
+             * path: Path to the array field.
+             * includeArrayIndex: Optional name for index.
+             * preserveNullAndEmptyArrays: Optional
+             *   toggle to unwind null and empty values.
              */
                 {
-                    _id: "$director",
-                    directorFilmViews: {
-                        $sum: "$viewCount"
-                    }
-                }
-        }
-    ])
-)
-
-
-printjson(
-    "--- Wyświetlenie filmów z największą liczbą wyświetleń dla filmu premium oraz nonpremium ---\n",
-    db.movies.aggregate([
-        {
-            $sort:
-            /**
-             * Provide any number of field/order pairs.
-             */
-                {
-                    viewCount: -1
+                    path: "$userReviews"
                 }
         },
         {
@@ -120,27 +66,91 @@ printjson(
              * fieldN: The first field name.
              */
                 {
-                    _id: "$premiumMovie",
-                    title: {
-                        $first: "$title"
+                    _id: "$title",
+                    criticsRating: {
+                        $first: "$reviewScore"
                     },
-                    viewCount: {
-                        $first: "$viewCount"
+                    avgUserRating: {
+                        $avg: "$userReviews.reviewScore"
                     }
                 }
-        },
-        {
-            $project:
-            /**
-             * specifications: The fields to
-             *   include or exclude.
-             */
-                {
-                    _id: 0,
-                    premiumMovie: "$_id",
-                    title: 1,
-                    viewCount: 1
-                }
         }
-    ])
+    ]).toArray()
+
+printjson(
+    "\n\n--- Ocena krytyków oraz średnia ocen użytkowników ---\n")
+console.table(avgRatingQuery);
+
+
+
+
+const directorViewsQuery = db.movies.aggregate([
+    {
+        $group:
+        /**
+         * _id: The id of the group.
+         * fieldN: The first field name.
+         */
+            {
+                _id: "$director",
+                directorFilmViews: {
+                    $sum: "$viewCount"
+                }
+            }
+    }
+]).toArray()
+
+printjson(
+    "\n\n--- Łączne wyświetlenia zdobyte przez filmy danych reżyserów ---\n"
 )
+
+console.table(directorViewsQuery);
+
+
+
+const highestViewsQuery = db.movies.aggregate([
+    {
+        $sort:
+        /**
+         * Provide any number of field/order pairs.
+         */
+            {
+                viewCount: -1
+            }
+    },
+    {
+        $group:
+        /**
+         * _id: The id of the group.
+         * fieldN: The first field name.
+         */
+            {
+                _id: "$premiumMovie",
+                title: {
+                    $first: "$title"
+                },
+                viewCount: {
+                    $first: "$viewCount"
+                }
+            }
+    },
+    {
+        $project:
+        /**
+         * specifications: The fields to
+         *   include or exclude.
+         */
+            {
+                _id: 0,
+                premiumMovie: "$_id",
+                title: 1,
+                viewCount: 1
+            }
+    }
+]).toArray()
+
+printjson(
+    "\n\n--- Wyświetlenie filmów z największą liczbą wyświetleń dla filmu premium oraz nonpremium ---\n"
+)
+
+console.table(highestViewsQuery);
